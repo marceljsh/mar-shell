010b3e39c0b86aacc0b300a92ee77571a926b595
Marcel Joshua <marceljoshua69@gmail.com>
2024-05-14 13:45:12 +0700

    add banner component

---
+++ components/cmd/banner.tsx
@@ -0,0 +1,45 @@
+const Banner = () => {
+  return (
+    <>
+      <pre className='w-full max-w-lg text-left [font-size:_clamp(8px,3vw,16px)] text-neon-black dark:text-neon-green leading-none'>
+        {`
+ ███▄ ▄███▓ ▄▄▄       ██▀███       ██████  ██░ ██ ▓█████  ██▓     ██▓    
+ ▓██▒▀█▀ ██▒▒████▄    ▓██ ▒ ██▒   ▒██    ▒ ▓██░ ██▒▓█   ▀ ▓██▒    ▓██▒    
+ ▓██    ▓██░▒██  ▀█▄  ▓██ ░▄█ ▒   ░ ▓██▄   ▒██▀▀██░▒███   ▒██░    ▒██░    
+ ▒██    ▒██ ░██▄▄▄▄██ ▒██▀▀█▄       ▒   ██▒░▓█ ░██ ▒▓█  ▄ ▒██░    ▒██░    
+ ▒██▒   ░██▒ ▓█   ▓██▒░██▓ ▒██▒   ▒██████▒▒░▓█▒░██▓░▒████▒░██████▒░██████▒
+ ░ ▒░   ░  ░ ▒▒   ▓▒█░░ ▒▓ ░▒▓░   ▒ ▒▓▒ ▒ ░ ▒ ░░▒░▒░░ ▒░ ░░ ▒░▓  ░░ ▒░▓  ░
+ ░  ░      ░  ▒   ▒▒ ░  ░▒ ░ ▒░   ░ ░▒  ░ ░ ▒ ░▒░ ░ ░ ░  ░░ ░ ▒  ░░ ░ ▒  ░
+ ░      ░     ░   ▒     ░░   ░    ░  ░  ░   ░  ░░ ░   ░     ░ ░     ░ ░   
+				░         ░  ░   ░              ░   ░  ░  ░   ░  ░    ░  ░    ░  ░
+				`
+          // I split each chars and give them different color
+          .split('\n')
+          .map((line, index) => (
+            <span key={index}>
+              {line.split('').map((char, index) =>
+                char === '░' ? (
+                  <span
+                    key={index}
+                    className='text-neon-red dark:text-neon-yellow'
+                  >
+                    ░
+                  </span>
+                ) : char === '▒' ? (
+                  <span
+                    key={index}
+                    className='text-neon-black dark:text-neon-green'
+                  >
+                    ▒
+                  </span>
+                ) : (
+                  char
+                )
+              )}
+              {'\n'}
+            </span>
+          ))}
+      </pre>
+    </>
+  )
+}
