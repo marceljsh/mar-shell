7c9ab3fb9a9b3a4258728377778cbc4b2c30d254
Marcel Joshua <marceljoshua69@gmail.com>
2024-05-14 13:53:31 +0700

    add header component

---
+++ components/cmd/header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+	return (
+		<code className='mr-2 dark:text-neon-yellow text-neon-black [font-size:_clamp(10px,3vw,14px)]'>
+			visitor@mar-shell:~$
+		</code>
+	)
+}
+
+export default Header
